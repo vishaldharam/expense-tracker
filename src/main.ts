df687aa861b4ebb96704f773a8b9b509e1cf13e8
Vishal Dharam <vishal.dharam20@pccoepune.org>
2025-03-08 09:32:32 +0530

    ds

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://expense-tracker-frontend-vishaldharams-projects.vercel.app/',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
